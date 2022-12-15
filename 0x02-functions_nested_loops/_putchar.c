d3a9e88733d9d53e6919abba5fb4b9d1b4f4b3a6
Osariemen Emokhare <demokhare@yahoo.com>
2022-12-14 21:26:55 -0800

    Library for ALX

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/*
+*	_putchar write character to stdio
+*	the character to print
+*	return on sucess is 1
+*	on failure -1 is returned
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
