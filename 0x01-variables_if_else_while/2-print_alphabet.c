91fcf6874a85186d8fb6c3dfb044684fac284383
Osariemen Emokhare <demokhare@yahoo.com>
2022-12-15 22:16:09 -0800

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+print alphabet in lower case
+followed by new line
+return success
+*/
+
+int main(){
+
+	char i;
+	
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+putchar(\n);
+return 0;
+}
+
+
